6efd9c3535736c3efc80c5a50be5031faf60c770
wangwenbin131416 <wangwenbin@creality.com>
2021-08-24 09:43:08 +0800

    feat: 🎸 生成圆柱
    Change-Id: Ib88b2b51a98502b0b3a67894d21ecdbf66860a13

--- mmesh/mmesh/create/createcylinder.cpp
+++ mmesh/mmesh/create/createcylinder.cpp
@@ -1,6 +1,58 @@
 #include "createcylinder.h"
+#include "mmesh/trimesh/trimeshutil.h"
+#define  PI 3.141592 
 
 namespace mmesh
 {
+
+	trimesh::TriMesh* createCylinder(float _radius, float _height)
+	{
+		trimesh::TriMesh* cylinderMesh = new trimesh::TriMesh();
+		int degrees = 4;
+		int  trianglesCount = int(360 / degrees);//每4度一个三角形，一共有90个三角形
+		float radian = degrees * (PI / 180.0);//4度换算成弧度
+	
+		for (int i=0;i< trianglesCount;i++)
+		{
+			//top face
+			trimesh::point point0(0, 0, _height);
+			trimesh::point point1(_radius * cos(i * radian), _radius * sin(i * radian), _height);
+			trimesh::point point2(_radius * cos((i+1)* radian), _radius *sin((i+1)*radian), _height);
+			int v = cylinderMesh->vertices.size();
+			cylinderMesh->vertices.push_back(point0);
+			cylinderMesh->vertices.push_back(point1);
+			cylinderMesh->vertices.push_back(point2);
+			cylinderMesh->faces.push_back(trimesh::TriMesh::Face(v, v + 1, v + 2));
+			//Side 1a
+			point0 = trimesh::point(_radius * cos(i * radian), _radius * sin(i * radian), _height);
+			point1 = trimesh::point(_radius * cos(i * radian), _radius * sin(i * radian), 0);
+			point2 = trimesh::point(_radius * cos((i + 1) * radian), _radius * sin((i + 1) * radian), _height);
+			v = cylinderMesh->vertices.size();
+			cylinderMesh->vertices.push_back(point0);
+			cylinderMesh->vertices.push_back(point1);
+			cylinderMesh->vertices.push_back(point2);
+			cylinderMesh->faces.push_back(trimesh::TriMesh::Face(v, v + 1, v + 2));
+			//#Side 1b
+			point0 = trimesh::point(_radius * cos(i * radian), _radius * sin(i * radian), 0);
+			point1 = trimesh::point(_radius * cos((i + 1) * radian), _radius * sin((i + 1) * radian), 0);
+			point2 = trimesh::point(_radius * cos((i + 1) * radian), _radius * sin((i + 1) * radian), _height);
+			v = cylinderMesh->vertices.size();
+			cylinderMesh->vertices.push_back(point0);
+			cylinderMesh->vertices.push_back(point1);
+			cylinderMesh->vertices.push_back(point2);
+			cylinderMesh->faces.push_back(trimesh::TriMesh::Face(v, v + 1, v + 2));
+			//#Bottom 
+			point0 = trimesh::point(0, 0, 0);
+			point1 = trimesh::point(_radius * cos(i * radian), _radius * sin(i * radian), 0);
+			point2 = trimesh::point(_radius * cos((i + 1) * radian), _radius * sin((i + 1) * radian), 0);
+			v = cylinderMesh->vertices.size();
+			cylinderMesh->vertices.push_back(point0);
+			cylinderMesh->vertices.push_back(point1);
+			cylinderMesh->vertices.push_back(point2);
+			cylinderMesh->faces.push_back(trimesh::TriMesh::Face(v, v + 1, v + 2));
+		}
+		mmesh::dumplicateMesh(cylinderMesh);
+		return cylinderMesh;
+	}
 
 }
--- mmesh/mmesh/create/createcylinder.h
+++ mmesh/mmesh/create/createcylinder.h
@@ -1,9 +1,10 @@
 #ifndef MMESH_CREATECYLINDER_1619866821989_H
 #define MMESH_CREATECYLINDER_1619866821989_H
+#include "trimesh2/TriMesh.h"
 
 namespace mmesh
 {
-
+	trimesh::TriMesh* createCylinder(float _radius, float _height);
 }
 
 #endif // MMESH_CREATECYLINDER_1619866821989_H
