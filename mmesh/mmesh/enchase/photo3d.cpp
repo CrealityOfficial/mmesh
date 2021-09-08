9db04f6cce167f922e76297af24686c8e8d38431
anoob <anoob@sina.cn>
2021-09-08 18:36:45 +0800

    photo3d

--- mmesh/mmesh/enchase/photo3d.cpp
+++ mmesh/mmesh/enchase/photo3d.cpp
@@ -99,11 +99,7 @@ namespace enchase
 		std::unique_ptr<trimesh::TriMesh> destPtr(generate(param, errorCode));
 		if (destPtr)
 		{
-#if _WIN32
 			destPtr->write(stlFile);
-#else
-			destPtr->write(stlFile, errorCode);
-#endif
 			return true;
 		}
 
