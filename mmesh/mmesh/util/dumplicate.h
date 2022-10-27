4292049f7ffdf14e2ac30ced1dc6e21d190beb51
wangjiang <wangjiang@creality.com>
2022-10-27 15:11:38 +0800

    fix:[mmesh]添加去除孤立顶点的接口
    Change-Id: I32e350c9d0411ac5610661b7afbc850beea044f6

--- mmesh/mmesh/util/dumplicate.h
+++ mmesh/mmesh/util/dumplicate.h
@@ -11,6 +11,9 @@ namespace mmesh
 {
 
 	void weldingMesh(trimesh::TriMesh* mesh, ccglobal::Tracer* tracer = nullptr);
+
+    //未用到的顶点用有效的顶点替换
+    void removeNorVector2(trimesh::TriMesh* mesh);
 }
 
 #endif // MMESH_MNODE_1622032440408_H
