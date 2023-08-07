07904cd02a2e7e8f6c9171092cb2fff7b9e3d49a
anoob <anoob@sina.cn>
2023-08-07 16:47:08 +0800

    feature:[checker]添加检测退化面的函数
    Change-Id: I464f5ab17f900a88861603904eb3cd0f6b3336ac

---
+++ mmesh/mmesh/util/checker.h
@@ -0,0 +1,10 @@
+#ifndef MMESH_CHECKER_1691396111581_H
+#define MMESH_CHECKER_1691396111581_H
+#include "trimesh2/TriMesh.h"
+
+namespace mmesh
+{
+	bool checkDegenerateFace(trimesh::TriMesh* mesh, bool remove = false);
+}
+
+#endif // MMESH_CHECKER_1691396111581_H
